6382aaa9ad3413ea2099b54c48d390d61d9de8a4
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-11-06 14:43:22 +0100

    add method

--- src/main/java/org/experis/course/Calculator.java
+++ src/main/java/org/experis/course/Calculator.java
@@ -1,2 +1,34 @@
-package com.experis.course;public class Calculator {
+package org.experis.course;
+
+public class Calculator {
+
+    // ATTRIBUTI
+    private Float firstNumber;
+    private Float secondNumber;
+
+    // GETTER E SETTER
+    public Float getFirstNumber() {
+        return firstNumber;
+    }
+
+    public Float getSecondNumber() {
+        return secondNumber;
+    }
+
+    // METODI
+    public float addNumber(float firstNumber, float secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public float subtractNumber(float firstNumber, float secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public float divideNumber(float firstNumber, float secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public float multiplyNumber(float firstNumber, float secondNumber) {
+        return firstNumber * secondNumber;
+    }
 }
